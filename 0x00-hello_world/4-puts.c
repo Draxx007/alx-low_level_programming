e7ebcab9531fd11dd49a68f745fac508763ddfbe
Davies <davolaposki3@gmail.com>
2022-12-08 16:53:17 -0800

    puts commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
 /**
- * main - printing a text with puts
- *
- * return: Always 0 (success)
- *
+ * main - Entry point  printing a text with puts
+ * Return: Always 0 (success)
  */
 int main(void)
 {
