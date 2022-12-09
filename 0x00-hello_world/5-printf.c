c59c969052f57119f15e65f809ff8fffc2e625b2
Davies <davolaposki3@gmail.com>
2022-12-08 17:04:07 -0800

    6th commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - print out text using printf
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,");
+	return (0);
+}
