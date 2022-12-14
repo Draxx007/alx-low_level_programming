a4f457fc3bed4da3c8b3c3d130a00b6bbce9e0c1
Davies <davolaposki3@gmail.com>
2022-12-14 05:20:23 -0800

    4th commit

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isalpha -> checks if the characters are uppcased
+ * @c: this houses the characters
+ * Return: returns 1 for uppercased characters and 0 for others
+ */
+
+int _isalpha(int c);
+{
+	return (c >= 'A' && c <= 'Z');
+}
