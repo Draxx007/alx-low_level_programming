2029665a04676899cfd2a1a63ca0459417dcc7e8
Davies <davolaposki3@gmail.com>
2022-12-14 05:28:50 -0800

    commmit for the 4th

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,7 +3,7 @@
 /**
  * _isalpha -> checks if the characters are uppcased
  * @c: this houses the characters
- * Return: returns 1 for uppercased characters and 0 for others
+ * Return: returns 0 or 1 depending on the condition
  */
 
 int _isalpha(int c);
