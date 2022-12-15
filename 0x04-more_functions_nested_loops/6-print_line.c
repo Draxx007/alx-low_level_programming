7bda42629d9623c8d0a897f034c1dc965527c476
Davies <davolaposki3@gmail.com>
2022-12-15 14:26:59 -0800

    my commit at home

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_line - prints lines
+ * @n: number of lines to be printed
+ * Return: 0
+ */
+
+void print_line(int n)
+{
+	int 1;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
